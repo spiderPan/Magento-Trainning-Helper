74f0eaf871c9446e291cd2e07c5ecde576a387ff
pan <bpan@northern.co>
2016-12-23 14:56:55 -0500

    loading extension script instead

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(info) {
+        console.table(info);
+        return {
+          redirectUrl: 'chrome-extension://eipjoagboddoaihiaacgnlnggekanmmg/js/Core_Compressed.js'
+       }
+    }, {
+        urls: [
+            "https://services.geolearning.com/courseware/scripts/Core_Compressed.js*",
+        ]
+    }, ["blocking"]
+);
